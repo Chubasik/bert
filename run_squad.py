07fa16084c39930faf4c808e9a66fc4dc6c78c2c
Chubasik <tsygankov.maxim@gmail.com>
2019-05-17 16:24:04 +0300

    Add squad sgd optimizer

--- run_squad.py
+++ run_squad.py
@@ -660,7 +660,7 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
       total_loss = (start_loss + end_loss) / 2.0
 
       train_op = optimization.create_optimizer(
-          total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
+          total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu, type='sgd')
 
       output_spec = tf.contrib.tpu.TPUEstimatorSpec(
           mode=mode,
